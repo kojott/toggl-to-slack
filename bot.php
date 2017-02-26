e0d74c53c69bfb47bd509a6c6c0dd9ef7cd3342f
Jiri George Dolejs <info@georgedolejs.com>
2017-02-26 18:10:41 +0100

    initial working version

---
+++ bot.php
@@ -0,0 +1,47 @@
+<?php
+
+define('APIKEY', '...'); #Add API key for Toggl
+define('APIURL', 'https://toggl.com/reports/api/v2/details');
+define('APIWORKSPACE', '...'); // ID of a workspace
+define('APICLIENT', '...'); // Company
+define('APISLACKURL', 'https://hooks.slack.com/services/...'); # add slack webhook URL
+define('USERNAME', 'George\'s bot');
+
+require_once 'functions.php';
+
+$date = date('Y-m-d');
+
+$toggl_data = get_data($date);
+if(count($toggl_data['data'])>0) {
+    foreach ($toggl_data['data'] as $record) {
+        $message .= '*' . $record['project'] . '* - ' . $record['description'] . ' - ' . gmdate("H:i:s", (int)$record['dur'] / 1000) . "\n";
+    }
+
+    # send data to slack channel
+    send_to_slack($message, $date);
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
+
+# get details of a record in time (change APIKEY)
+curl -v -u 465462406f568b4dfb63b5ad07454e72:api_token \
+    -X GET "https://toggl.com/reports/api/v2/details?workspace_id=111&client_ids=111&since=2017-02-24&until=2017-02-24&user_agent=api_test"
+
+
+# get clients (change APIKEY)
+curl -v -u 465462406f568b4dfb63b5ad07454e72:api_token \
+    -X GET https://www.toggl.com/api/v8/clients
+
+/**/
+
+
---
+++ functions.php
@@ -0,0 +1,59 @@
+<?php
+
+
+function get_data($date){
+    $url = APIURL.'?workspace_id='.APIWORKSPACE.'&client_ids='.APICLIENT.'&since='.$date.'&until='.$date.'&user_agent=api_test&order_field=date&order_desc=off';
+    $ch = curl_init($url);
+
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1);
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_USERPWD, APIKEY . ':api_token');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    //Execute the request
+    $result = curl_exec($ch);
+
+    return json_decode($result, true);
+
+}
+
+
+function send_to_slack($message, $date){
+    //API Url
+    $url = APISLACKURL;
+
+    //Initiate cURL.
+    $ch = curl_init($url);
+
+    # The JSON data.
+    $payload = array(
+        'username' => USERNAME,
+        "icon_emoji" => ":run:",
+        "channel" => "#general",
+        'text' => 'George\'s work on '.$date."\n\n".$message,
+        "mrkdwn" => true,
+    );
+
+    //Encode the array into JSON.
+    $jsonDataEncoded = json_encode($payload);
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1);
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+    //Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    //Execute the request
+    $result = curl_exec($ch);
+
+}
+
+function secondsToTime($seconds) {
+    $dtF = new \DateTime('@0');
+    $dtT = new \DateTime("@$seconds");
+    return $dtF->diff($dtT)->format('%h:%i:%s');
+}
